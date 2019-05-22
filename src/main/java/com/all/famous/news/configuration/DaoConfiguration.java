121e70c170a3e8bf3d683a875d6dfdd7bc7ba67e
Uladzislau-Hrytsau <vladboy45@gmail.com>
2019-05-22 20:32:38 +0300

    h2

---
+++ src/main/java/com/all/famous/news/configuration/DaoConfiguration.java
@@ -0,0 +1,30 @@
+package com.all.famous.news.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DaoConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("jdbc:h2:mem:test_db;MODE=MYSQL;DB_CLOSE_DELAY=-1")
+                .addScript("sql/schema.sql")
+                .addScript("sql/data.sql").build();
+    }
+
+    @Bean
+    @Autowired
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+}
---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `category` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(45) NOT NULL,
+  `parent_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `category_name_UNIQUE` (`category_name`),
+  KEY `parent_id` (`parent_id`),
+  CONSTRAINT `fk_category_1` FOREIGN KEY (`parent_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `article` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `article_name` varchar(45) NOT NULL,
+  `article_description` varchar(512) NOT NULL,
+  `article_content` varchar(4096) NOT NULL,
+  `encode_image` varchar(45) NOT NULL,
+  `category_id` int(10) unsigned NOT NULL,
+  `date` date NOT NULL,
+  `author` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_article_1_idx` (`category_id`),
+  CONSTRAINT `fk_article_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8;
